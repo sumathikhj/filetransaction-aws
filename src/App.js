2328adcd0b57b72ff92655e63a8342a0e8c0948c
sumathikhj <nsumathi2001@gmail.com>
2022-03-07 21:16:17 +0530

    edited app

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+        Happy Learning React
         </a>
       </header>
     </div>
